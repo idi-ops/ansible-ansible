b62ef12716ad6937bc42a2fd8c2a64a6edd8a91b
Tyler Roscoe <tyler@cryptio.net>
2017-03-09 17:25:22 -0700

    Uninstall yum ansible packages. Install pip from yum.

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,12 +1,18 @@
 import testinfra.utils.ansible_runner
 
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
+def test_yum_packages_not_installed(Package):
+    ansible_pkg = Package("ansible")
+    assert not ansible_pkg.is_installed
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    jinja_pkg = Package("python-jinja2")
+    assert not jinja_pkg.is_installed
+
+
+def test_pip_installed(Package):
+    pip_pkg = Package("python2-pip")
+    assert pip_pkg.is_installed
